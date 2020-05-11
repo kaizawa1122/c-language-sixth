bd9ddcc8979b1a0cfe99b53d6093a0dc3944b0e6
kaizawa1122 <kai109870@gmail.com>
2020-05-11 22:51:35 +0900

    add files 6-1-3.c

---
+++ 6-1-3.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXWORD 100
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char *,int);
+
+int main()
+{
+	struct tnode *root;
+	char word[MAXWORD];
+
+	root = NULL;
+	while (getword(word,MAXWORD) != EOF)
+	{
+		if (isalpha(word[0]))
+		{
+			root = addtree(root, word);
+		}
+	}
+		treeprint(root);
+		return 0;
+}
+
+struct tnode *talloc(void);
+char *strdup01(char *s);
+
+struct tnode *addtree(struct tnode *p, char *w)
+{
+	int cond;
+
+	if (p == NULL)
+	{
+		p = talloc();
+		p->word = strdup01(w);
+		p->count = 1;
+		p->left = p->right = NULL;
+	}
+	else if ((cond = strcmp(w, p->word)) == 0)
+	{
+		p->count++;
+	}
+	else if (cond < 0)
+	{
+		p->left = addtree(p->left,w);
+	}
+	else
+	{
+		p->right = addtree(p->right,w);
+	}
+	return p;
+}
+
+struct tnode *talloc(void)
+{
+	return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *strdup01(char *s)
+{
+	char *p;
+
+	p = (char *) malloc(strlen(s) + 1);
+	if (p != NULL)
+	{
+		strcpy(p,s);
+	}
+	return p;
+}
